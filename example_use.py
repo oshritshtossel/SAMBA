b2f1024df2819c86e6895e10c78fe72b3fe217b1
oshritshtossel <oshritvig@gmail.com>
2024-06-26 22:21:45 +0300

    update

--- example_use.py
+++ example_use.py
@@ -8,6 +8,7 @@ from src.samba import micro2matrix
 from src.samba import build_SAMBA_distance_matrix, plot_umap
 
 if __name__ == '__main__':
+    CLASS = False
     # Load the raw data in the required format
     df = pd.read_csv("example_data/for_preprocess.csv")
     tag = pd.read_csv("example_data/tag.csv", index_col=0)
@@ -20,9 +21,9 @@ if __name__ == '__main__':
     array_of_imgs,bact_names, ordered_df = micro2matrix(processed, folder, save=False)
 
     # Calculate the distance matrix according to SAMBA
-    DM = build_SAMBA_distance_matrix(folder,imgs=array_of_imgs,ordered_df=ordered_df)
-
-    DM.to_csv("result.csv")
+    DM = build_SAMBA_distance_matrix(folder,imgs=array_of_imgs,ordered_df=ordered_df,bact_names=bact_names,class_=CLASS)
 
     # Plot UMAP according to the distance matrix and some tag
     plot_umap(DM, tag, "example_data")
+
+
